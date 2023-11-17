8a3e5c8b2d4ffec1ae17f39f4322505105146a90
annaLucian <garielapoves01@gmail.com>
2023-11-17 16:31:15 +0100

    cambiar estilos, agregar interaccion en js

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,33 @@
+//constantes colores
+const root = document.documentElement;
+const colorVariableRed = getComputedStyle(root).getPropertyValue("--color-red")
+const colorVariableBlack = getComputedStyle(root).getPropertyValue("--color-black")
+const colorVariableGray = getComputedStyle(root).getPropertyValue("--color-gray")
 
+//constantes botones 
 const colorRed = document.querySelector(".red")
-const colorBlack = document.getElementsByClassName("black")
-const colorGray =  document.getElementsByClassName("gray")
-
+const colorBlack = document.querySelector(".black")
+const colorGray = document.querySelector(".gray")
+//selector de imagen
+const container = document.querySelector(".product-description")
 const imgCard = document.querySelector(".product-image")
+//selector tag
+const tagSelector = document.querySelector(".tag")
+//selector de boton
+const btnAdd = document.querySelector(".button-add-cart")
 
-const itemTag = document.getElementsByTagName("h3")
+const changeColorRed = () => {
+  tagSelector.style.backgroundColor = colorVariableRed
+  btnAdd.style.backgroundColor = colorVariableRed
+  imgCard.style.backgroundImage = 'url("../../public/img/redcar.jpg")'
+  container.style.backgroundColor = "rgb(223 142 0)"
+}
 
-const cartButton = document.getElementById("button")
+const changeColorGray = () => {
+  tagSelector.style.backgroundColor = colorVariableGray
+  btnAdd.style.backgroundColor = colorVariableGray
+  imgCard.style.backgroundImage = 'url("../../public/img/graycar.jpg")'
+}
 
-colorRed.addEventListener("click", () =>{
-  imgCard.style.backgroungImage = "url('../public/img/redcar.jpg')";
-  cartButton.style.backgroundColor = "red";
-  itemTag.style.backgroundColor = "red";
-});
+colorRed.addEventListener('click', changeColorRed)
+colorGray.addEventListener('click', changeColorGray)
