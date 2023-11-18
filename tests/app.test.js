87156ed729f987ab4179fc1c2126aec1bda0676f
annaLucian <garielapoves01@gmail.com>
2023-11-18 18:13:33 +0100

    Implementando testing

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,30 +1,39 @@
 
-import { describe, expect, it, beforeAll } from 'vitest'
+import { describe, expect, it, beforeAll, vi } from 'vitest'
 import { JSDOM } from 'jsdom'
 import { changeStyle } from '../src/js/app.js'
-// const { changeStyle } = require('../src/js/app.js');
+
 describe('APP', () => {
-  let dom;
+  let dom
+  let document
+  let backgroundMock
+  let productImgeMock
+  let tagMock
+  let buttonAddCartMock
+  let btnChangeToBlackMock
+  let mockChangeStyleFunc
   beforeAll(async () => {
     dom = await JSDOM.fromFile("index.html", {
       resources: "usable",
       runScripts: "dangerously",
-    });
+    })
 
-    tagMock = document.createElement("body");
-    tagMock.classList.add("tag");
-    document.body.appendChild(tagMock)
+    document = dom.window.document
+    backgroundMock = dom.window.document.querySelector('.product-description')
+    mockChangeStyleFunc = vi.fn().mockImplementation((styles) => {
+      backgroundMock.style.backgroundColor = styles
+    })
+
+
   });
 
   it("should render css", async () => {
-    let document = dom.window.document;
 
     let link = document.querySelector("link");
     expect(link.href).toMatch(/\/src\/css\/style.css$/);
   });
 
   it("should render js file", async () => {
-    let document = dom.window.document;
 
     let script = document.querySelector("script");
     expect(script.src).toMatch(/\/src\/js\/app.js$/);
@@ -35,12 +44,16 @@ describe('APP', () => {
     expect(typeof changeStyle).toBe("function");
   });
 
-  it.only("changeStyle changes the label, the add button and the background color of the button color", async () => {
-    backgroundMock.style.backgroundColor = "rgb(255, 0, 0)";
-    changeStyles();
-    expect(backgroundMock.style.backgroundColor).toBe("darkblue");
-    changeStyles();
-    expect(backgroundMock.style.backgroundColor).toBe("red");
+  it.only("should changeStyle changes the label, the add button and the background color of the button color", async () => {
+
+    //simulate click on button function changeStyles
+
+    mockChangeStyleFunc("red")
+
+    console.log("despues", { log: backgroundMock.style.backgroundColor });
+
+    expect(backgroundMock.style.backgroundColor).toBe("red")
+
   });
 
 })
